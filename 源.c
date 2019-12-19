698649d90478586fc67d712351e91b771ee94128
飞 <1752840757@qq.com>
2019-12-19 22:33:50 +0800

    源

---
+++ 源.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int* p = arr;
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", *(p + i));
+	}
+	system("pause");
+	return 0;
+}
