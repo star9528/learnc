a8b2dbda0df945d16b8c3fe601b84bc7b70e12f4
飞 <1752840757@qq.com>
2019-12-26 23:42:58 +0800

    判断

---
+++ Judge.java
@@ -0,0 +1,10 @@
+public class Judge{
+    public static void main(String[] args) {
+        int num =10;
+        if(num%2==0){
+            System.out.println("num 是偶数");
+        } else {
+            System.out.println("num 是奇数");
+        }
+    }
+}
