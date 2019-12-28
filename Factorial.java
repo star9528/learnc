ed55f5d7312ef8cf0519bb28c531c6dd91f3df8c
飞 <1752840757@qq.com>
2019-12-28 22:08:27 +0800

    阶乘

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial{
+    public static int factorial(int num){
+        if(num==1){
+            return 1;
+        }
+        else {
+            return num*factorial(num-1);
+        }
+       }
+    public static void main(String[] args) {
+        int num=5;
+        System.out.println(factorial(num));
+    }
+}
