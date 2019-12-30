13dbbebe91a2e320e1a65b9455d8f434c22aea48
飞 <1752840757@qq.com>
2019-12-30 22:52:12 +0800

    判断正负

---
+++ Judge2.java
@@ -0,0 +1,11 @@
+public class Judge2{
+    public static void main(String[] args) {
+        int num=10;
+        if(num>0){
+            System.out.println("这是一个整数！");
+        }
+        else {
+            System.out.println("这是一个负数");
+        }
+    }
+}
